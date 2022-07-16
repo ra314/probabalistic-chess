e57c2ac0847754799fe3019e79e18f9e1688fd35
ra314 <rahulgandharva20122000@gmail.com>
2022-07-16 20:52:41 +1000

    Refactored rook movement.

--- Rook.gd
+++ Rook.gd
@@ -9,45 +9,36 @@ func _ready():
 	board = get_parent()
 	pass # Replace with function body.
 
+var x: int
+var y: int
+var pos_to_check: Vector2
 func generate_legal_moves() -> Array:
 	var legal_moves := []
 	
-	print(board)
-	
-	# Move RIGHT
-	var x := grid_pos[0]+1
-	var pos_to_check := Vector2(x, grid_pos[1])
-	while board.is_in_grid(pos_to_check):
-		if board.get_tile(pos_to_check) == null:
-			legal_moves.append(pos_to_check)
-		x += 1
-		pos_to_check = Vector2(x, grid_pos[1])
-	
-	# Move LEFT
-	x = grid_pos[0]-1
-	pos_to_check = Vector2(x, grid_pos[1])
-	while board.is_in_grid(pos_to_check):
-		if board.get_tile(pos_to_check) == null:
-			legal_moves.append(pos_to_check)
-		x -= 1
-		pos_to_check = Vector2(x, grid_pos[1])
-	
-	# Move UP
-	var y := grid_pos[1]+1
-	pos_to_check = Vector2(grid_pos[0], y)
-	while board.is_in_grid(pos_to_check):
-		if board.get_tile(pos_to_check) == null:
-			legal_moves.append(pos_to_check)
-		y += 1
-		pos_to_check = Vector2(grid_pos[0], y)
-	
-	# Move DOWN
-	y = grid_pos[1]-1
-	pos_to_check = Vector2(grid_pos[0], y)
-	while board.is_in_grid(pos_to_check):
-		if board.get_tile(pos_to_check) == null:
-			legal_moves.append(pos_to_check)
-		y -= 1
-		pos_to_check = Vector2(grid_pos[0], y)
+	for method_name in ["move_N", "move_S", "move_E", "move_W"]:
+		call(method_name)
+		set_pos_to_check()
+		while board.is_in_grid(pos_to_check):
+			if board.get_tile(pos_to_check) == null:
+				legal_moves.append(pos_to_check)
+			call(method_name)
+			set_pos_to_check()
+		x = 0
+		y = 0
 	
 	return legal_moves
+
+func set_pos_to_check():
+	pos_to_check = Vector2(x, y)
+
+func move_N():
+	y+=1
+
+func move_S():
+	y-=1
+
+func move_E():
+	x+=1
+
+func move_W():
+	x-=1
