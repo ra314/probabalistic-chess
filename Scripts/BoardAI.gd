ebbeb601d739cda134bc4764e127bde0aaa55ca4
ra314 <rahulgandharva20122000@gmail.com>
2022-08-01 02:53:36 -0700

    Added move ordering.

--- Scripts/BoardAI.gd
+++ Scripts/BoardAI.gd
@@ -17,6 +17,30 @@ func get_all_legal_moves(color: bool):
 			legal_moves.append([piece.grid_pos, legal_move])
 	return legal_moves
 
+func order_moves_sorter_helper(move1, move2) -> bool:
+	return move_eval_guesses[move1] > move_eval_guesses[move2]
+
+var move_eval_guesses := {}
+
+# Pass in a duplicated array if you don't want the original array to mutate
+# Moves that are heuristically evaluated to be better are at the start of the returned array
+func order_moves(moves: Array) -> Array:
+	move_eval_guesses = {}
+	for move in moves:
+		var eval = 0
+		
+		# Reward captures
+		var killed_piece = FBP.pieces_grid.get(move[1])
+		if killed_piece != null:
+			var piece_to_move = FBP.pieces_grid[move[0]]
+			var chance_of_success = piece_to_move.odds_of_successful_attack(killed_piece)
+			eval += (killed_piece.eval_value * chance_of_success)
+		
+		move_eval_guesses[move] = eval
+	
+	moves.sort_custom(self, "order_moves_sorter_helper")
+	return moves
+
 var mm_node = {"move": null, "eval": 0}
 
 var num_nodes_evaluated = 0
@@ -24,7 +48,6 @@ func evaluate_MM_root(max_depth: int, maximising: bool) -> Array:
 	FBP = BP.duplicate()
 	BP.duplicate_board(FBP)
 	
-	#var eval = evaluate_MM(1, max_depth, maximising, -INF, +INF)
 	var eval = evaluate_MM4(max_depth, maximising, -INF, +INF)
 	debug_print_eval_and_moves(eval)
 	print(str(num_nodes_evaluated) + " nodes were evaluated at a depth of " + str(max_depth))
@@ -46,7 +69,7 @@ func evaluate_MM4(depth: int, maximising: bool, alpha: float, beta: float) -> Ar
 		best_eval = INF
 	var best_moves = null
 	
-	for legal_move in get_all_legal_moves(maximising):
+	for legal_move in order_moves(get_all_legal_moves(maximising)):
 		# Playing the proposed move
 		var killed_piece = FBP.pieces_grid.get(legal_move[1])
 		var chance_of_success := 1.0
@@ -60,6 +83,10 @@ func evaluate_MM4(depth: int, maximising: bool, alpha: float, beta: float) -> Ar
 		var eval_and_moves = evaluate_MM4(depth-1, !maximising, alpha, beta)
 		var eval = eval_and_moves[0]
 		var moves = eval_and_moves[1]
+		
+		# TODO
+		# Counter the eval given a capture
+		# with the eval given that the current play makes not move due to a failed capture
 		
 		# Updating alpha, beta
 		if maximising:
@@ -100,5 +127,4 @@ func debug_print_eval_and_moves(eval_and_moves):
 	print_str += str(eval_and_moves[0]) + ": "
 	for move in eval_and_moves[1]:
 		print_str += debug_print_move(move)
-	print_str += "\n"
 	print(print_str)
